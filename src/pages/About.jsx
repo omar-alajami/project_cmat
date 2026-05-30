cbe7ed328a93ee578551299d5c311ec4f7c2ad1f
Omar Al-Ajami <omar.r.ajami@gmail.com>
2026-05-30 22:29:04 +0300

    finishing touches

--- src/pages/About.jsx
+++ src/pages/About.jsx
@@ -1,7 +1,31 @@
 function About(){
     return(
         <div className="container mt-5">
-            
+            <p>
+                We're a small group of computer science students who've created this website
+                after struggling to find review material for the courses we're taking.
+                <br />
+                All the material available on this website are provided by LIU students.
+                <br /> <br />
+                In case you run into any problem, you can contact us at 22430396@students.liu.edu.lb and we'll reply as soon as possible.
+                <br /> <br />
+                This website is built as a project for the "CSCI390 - Web Development" course.
+                <br />
+                We developed this website using React.js + Vite.
+                <br />
+                This website is open-source and does not have a backend as of now (May 2026).
+                <br />
+                You can find the project on GitHub at: <a href="https://www.github.com/omar-alajami/project_cmat.git">https://www.github.com/omar-alajami/project_cmat.git</a>
+                <br /><br />
+                Our team consists of:
+                <br />
+                Omar R. Al-Ajami.
+                <br /><br />
+                That's it I don't have a partner. I've been refering to myself with "we" just to sound more professional.
+                <br /><br /><br />
+                If you can't tell yet, I have no idea what I should put here. But it's not like anyone will read this much into the about page.
+                <br />
+            </p>
         </div>
     )
 }
